6eef83d74fff88c94f2d737d85cb687226609aa5
Ashish Upadhyay <upadhyayashish19204@gmail.com>
2026-04-05 18:19:34 +0530

    chat_repository.dart added, chat_message.dart in models added

---
+++ lib/data/repositories/chat_repository.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:messenger/data/models/chat_room_model.dart';
+import 'package:messenger/data/services/base_repository.dart';
+
+class ChatRepository extends BaseRepository{
+  CollectionReference get _chatRooms => firestore.collection("chatRooms");
+
+  CollectionReference getChatRoomMessages(String chatRoomId) {
+    return _chatRooms.doc(chatRoomId).collection("messages");
+  }
+
+  Future<ChatRoomModel> getOrCreateChatRoom(
+      String currentUserId, String otherUserId) async {
+    // Prevent creating a chat room with yourself
+    if (currentUserId == otherUserId) {
+      throw Exception("Cannot create a chat room with yourself");
+    }
+
+    final users = [currentUserId, otherUserId]..sort();
+    final roomId = users.join("_");
+
+    final roomDoc = await _chatRooms.doc(roomId).get();
+
+    if (roomDoc.exists) {
+      return ChatRoomModel.fromFirestore(roomDoc);
+    }
+
+    final currentUserData =
+        (await firestore.collection("users").doc(currentUserId).get()).data()
+            as Map<String, dynamic>;
+    final otherUserData =
+        (await firestore.collection("users").doc(otherUserId).get()).data()
+            as Map<String, dynamic>;
+    final participantsName = {
+      currentUserId: currentUserData['fullName']?.toString() ?? "",
+      otherUserId: otherUserData['fullName']?.toString() ?? "",
+    };
+
+    final newRoom = ChatRoomModel(
+        id: roomId,
+        participants: users,
+        participantsName: participantsName,
+        lastReadTime: {
+          currentUserId: Timestamp.now(),
+          otherUserId: Timestamp.now(),
+        });
+
+    await _chatRooms.doc(roomId).set(newRoom.toMap());
+    return newRoom;
+  }
+}
